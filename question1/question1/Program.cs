88a790346e2594bc997b4ed823bb2c5bcc07fc56
Ravindushamentha <118343030+Ravindushamentha@users.noreply.github.com>
2023-07-31 23:37:17 +0530

    Add files via upload

---
+++ question1/question1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace KilometerToMeterConverter
+{
+    class ConvertValues
+    {
+        public void KilometerToMeter()
+        {
+            Console.Write("Enter the value in kilometers (km): ");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double kilometers))
+            {
+                double meters = kilometers * 1000;
+                Console.WriteLine($"The value in meters (m) is: {meters} m");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number for kilometers.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Kilometer to Meter Converter!");
+
+            // Create an object of the ConvertValues class
+            ConvertValues converter = new ConvertValues();
+
+            // Call the KilometerToMeter method to perform the conversion
+            converter.KilometerToMeter();
+
+            // Keep the console window open until the user presses a key
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
